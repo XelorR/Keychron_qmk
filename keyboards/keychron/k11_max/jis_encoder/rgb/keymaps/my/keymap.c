037bd8bad599df500d916a1ad509d7fbdfb03ad3
Petr <petr@duck.com>
2025-04-13 16:02:40 +0300

    initiating layout design

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/my/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/my/keymap.c
@@ -20,6 +20,46 @@
 #define LANG_ENG C(KC_8)
 #define LANG_RUS C(KC_9)
 
+// Layers
+#define SPC_NAV LT(NAVIGATION,KC_SPC)
+#define ENT_MAC LT(MACRO,KC_ENT)
+
+// Mod-taps
+#define HG_A LGUI_T(KC_A)
+#define HA_R LALT_T(KC_R)
+#define HC_S LCTL_T(KC_S)
+#define HS_T LSFT_T(KC_T)
+#define HS_N RSFT_T(KC_N)
+#define HC_E LCTL_T(KC_E)
+#define HA_I LALT_T(KC_I)
+#define HG_O LGUI_T(KC_O)
+
+#define HG_E LGUI_T(KC_E)
+#define HA_B LALT_T(KC_B)
+#define HC_T LCTL_T(KC_T)
+#define HS_J LSFT_T(KC_J)
+#define HS_Y RSFT_T(KC_Y)
+#define HC_N LCTL_T(KC_N)
+#define HA_C LALT_T(KC_C)
+#define HG_H LGUI_T(KC_H)
+
+// Modifiers to shortcut layer
+#define SH_LCTL LM(SHORTCUT,MOD_LCTL)
+#define SH_RCTL LM(SHORTCUT,MOD_RCTL)
+#define SH_LWIN LM(SHORTCUT,MOD_LWIN)
+#define SH_RWIN LM(SHORTCUT,MOD_RWIN)
+#define SH_LALT LM(SHORTCUT,MOD_LALT)
+#define SH_RALT LM(SHORTCUT,MOD_RALT)
+
+// CUA
+
+#define UNDO  C(KC_Z)
+#define CUT   C(KC_X)
+#define COPY  C(KC_C)
+#define PASTE C(KC_V)
+#define REDO1 C(S(KC_Z))
+#define REDO2 C(KC_Y)
+
 enum custom_keycodes {
     LG_ENG = NEW_SAFE_RANGE, // layer and system layout to english - 64
     LG_RUS,  // layer and system layout to russian - 65
@@ -92,58 +132,67 @@ enum custom_keycodes {
 };
 
 enum layers {
-    MAC_BASE,
-    WIN_BASE,
-    MAC_FN1,
-    WIN_FN1,
-    FN2,
+    ENGLISH_COLEMAC,
+    RUSSIAN_DIKTOR,
+    SHORTCUT,
+    GAMING,
+    NAVIGATION,
+    MACRO,
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [MAC_BASE] = LAYOUT_73_jis(
-        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_INT3,  KC_BSPC,   KC_MUTE,
+    [ENGLISH_COLEMAC] = LAYOUT_73_jis(
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSPC,  KC_MUTE,
+        KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,                      KC_DEL,
+        KC_ESC,   HG_A,     HA_R,     HC_S,     HS_T,     KC_D,               KC_H,     HS_N,     HC_E,     HA_I,     HG_O,     KC_QUOT,  KC_BSLS,  ENT_MAC,  KC_HOME,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_RSFT,  KC_UP,
+        SH_LCTL,  SH_LWIN,  SH_RWIN,  KC_LALT,  SPC_NAV,  LG_ENG,             LG_RUS,   SPC_NAV,  SH_RALT,  LG_GAME,            KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [RUSSIAN_DIKTOR] = LAYOUT_73_jis(
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSPC,  KC_MUTE,
+        KC_TAB,   KC_W,     KC_M,     KC_Z,     KC_QUES,  KC_SLSH,  KC_P,     KC_D,     KC_R,     KC_L,     KC_X,     KC_I,     KC_O,                         KC_DEL,
+        KC_ESC,   HG_E,     HA_B,     HC_T,     HS_J,     KC_F,               KC_K,     HS_Y,     HC_N,     HA_C,     HG_H,     KC_Q,     KC_BSLS,  ENT_MAC,  KC_HOME,
+        KC_LSFT,  KC_Z,     KC_X,     KC_LBRC,  KC_S,     KC_DOT,   KC_RBRC,  KC_COMM,  KC_V,     KC_G,     KC_U,     KC_SCLN,  KC_RSFT,  KC_RSFT,  KC_UP,
+        SH_LCTL,  SH_LWIN,  SH_RWIN,  KC_LALT,  SPC_NAV,  LG_ENG,             LG_RUS,   SPC_NAV,  SH_RALT,  LG_GAME,            KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [SHORTCUT] = LAYOUT_73_jis(
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSPC,  KC_MUTE,
+        KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,                      KC_DEL,
+        KC_CAPS,  KC_A,     KC_R,     KC_S,     KC_T,     KC_D,               KC_H,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,  KC_BSLS,  KC_ENT,   KC_HOME,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_RSFT,  KC_UP,
+        KC_LCTL,  KC_LWIN,  KC_RCMMD, KC_LALT,  KC_SPC,   LG_ENG,             LG_RUS,   KC_SPC,   KC_RALT,  LG_GAME,            KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [GAMING] = LAYOUT_73_jis(
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSPC,  KC_MUTE,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                      KC_DEL,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,  KC_ENT,   KC_HOME,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_INT1,  KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LOPTN, KC_LCMMD, KC_LNG2,  KC_SPC,          MO(MAC_FN1), MO(FN2),            KC_SPC,   KC_LNG1,  KC_RCMMD,           KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_RSFT,  KC_UP,
+        KC_LCTL,  KC_LWIN,  KC_LCMMD, KC_LALT,  KC_SPC,   LG_ENG,             LG_RUS,   KC_SPC,   KC_RALT,  LG_GAME,            KC_LEFT,  KC_DOWN,  KC_RGHT),
 
-    [WIN_BASE] = LAYOUT_73_jis(
-        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_INT3,  KC_BSPC,  KC_MUTE,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                      KC_DEL,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,  KC_ENT,   KC_HOME,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_INT1,  KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LWIN,  KC_LALT,  KC_INT5,  KC_SPC,          MO(WIN_FN1), MO(FN2),            KC_SPC,   KC_INT4,  KC_RALT,            KC_LEFT,  KC_DOWN,  KC_RGHT),
+    [NAVIGATION] = LAYOUT_73_jis(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,
+        KC_TAB,  A(KC_F4), C(S(KC_TAB)), KC_TAB, C(KC_TAB), _______,          KC_ESC,   KC_HOME,  KC_UP,    KC_END,   KC_DEL,   _______,  _______,            MS_WHLU,
+        KC_ESC,   KC_LWIN, KC_LALT, OSM(MOD_LCTL), KC_LSFT, KC_PSCR,        S(KC_RSFT), KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_BSPC,  _______,  _______,  KC_ENT,   MS_WHLD,
+        KC_RSFT,  UNDO,     CUT,      COPY,     PASTE,    REDO1,    REDO2,    G(KC_E),  KC_PGUP,  KC_APP,   KC_PGDN,  KC_ENT,   MS_BTN2,  MS_BTN1,  MS_UP,
+        KC_RCTL,  KC_RWIN,  KC_RCMMD, KC_RALT,  KC_SPC,             LG_RUS,   LG_RUS,             KC_SPC,   KC_RALT,  LG_GAME,            MS_LEFT,  MS_DOWN,  MS_RGHT),
 
-    [MAC_FN1] = LAYOUT_73_jis(
-        KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  RGB_TOG,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      KC_INS,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_END,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,
-        _______,  _______,  _______,  _______,  _______,            _______,  _______,            _______,  _______,  _______,            _______,  KC_PGDN,  _______),
-
-    [WIN_FN1] = LAYOUT_73_jis(
+    [MACRO] = LAYOUT_73_jis(
         KC_GRV,   KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  RGB_TOG,
         _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      KC_INS,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_END,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,
-        _______,  _______,  _______,  _______,  _______,            _______,  _______,            _______,  _______,  _______,            _______,  KC_PGDN,  _______),
-
-    [FN2] = LAYOUT_73_jis(
-        KC_TILD,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
-        _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  BAT_LVL,  BAT_LVL,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,            _______,  _______,            _______,  _______,  _______,            _______,  _______,  _______)
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,            _______,  _______,  _______,  _______,  _______, QK_MACRO_1, _______, KC_ENT,   KC_END,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,
+        _______,  _______,  _______,  _______,  _______,            _______,  _______,            _______,  _______,  _______,            _______,  KC_PGDN,  _______)
 };
 
 #if defined(ENCODER_MAP_ENABLE)
     const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-        [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-        [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-        [MAC_FN1]  = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-        [WIN_FN1]  = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-        [FN2]      = { ENCODER_CCW_CW(_______, _______)},
+        [ENGLISH_COLEMAC] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+        [RUSSIAN_DIKTOR] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+        [SHORTCUT] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+        [GAMING] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+        [NAVIGATION] = { ENCODER_CCW_CW(_______, _______)},
+        [MACRO]  = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     };
 #endif // ENCODER_MAP_ENABLE
 
