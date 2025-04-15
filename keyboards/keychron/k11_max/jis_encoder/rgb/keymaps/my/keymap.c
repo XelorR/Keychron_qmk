da063b9fde7cda46cfc127922c198057d4132feb
Petr <petr@duck.com>
2025-04-15 22:41:06 +0300

    diktor symbols implemented, more symbol used in nav

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/my/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/my/keymap.c
@@ -71,9 +71,18 @@ enum custom_keycodes {
     CO_EMDASH, // — 99
     CO_DEGREE, // ° 100
 
-    // Punctuation
-    SUPER_DOT, // . ! — 101
-    SUPER_COMMA, // , ? «» “” 102
+    // Diktor
+    DI_1,
+    DI_2,
+    DI_4,
+    DI_5,
+    DI_6,
+    DI_7,
+    DI_8,
+    DI_STAR,
+    DI_DOT,
+    DI_COMMA,
+    DI_HARD,
 
     // Launchers
     TERMINAL,
@@ -98,8 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         SH_LCTL,  SH_LWIN,  SH_RWIN,  KC_LALT,  SPC_NAV,  LG_ENG,             LG_RUS,   SPC_NAV,  SH_RALT,  LG_GAME,            KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [RUSSIAN_DIKTOR] = LAYOUT_73_jis(
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSPC,  KC_MUTE,
-        KC_TAB,   KC_W,     KC_M,     KC_Z,     KC_QUES,  KC_SLSH,  KC_P,     KC_D,     KC_R,     KC_L,     KC_X,     KC_I,     KC_O,                         KC_DEL,
+        KC_GRV,   DI_1,     DI_2,     KC_3,     DI_4,     DI_5,     DI_6,     DI_7,     DI_8,     KC_9,     KC_0,     KC_STAR,  KC_EQL,   KC_BSPC,  KC_BSPC,  KC_MUTE,
+        KC_TAB,   KC_W,     DI_HARD,  KC_Z,     DI_COMMA, DI_DOT,   KC_P,     KC_D,     KC_R,     KC_L,     KC_X,     KC_I,     KC_O,                         KC_DEL,
         KC_ESC,   HG_E,     HA_B,     HC_T,     HS_J,     KC_F,               KC_K,     HS_Y,     HC_N,     HA_C,     HG_H,     KC_Q,     KC_BSLS,  ENT_MAC,  KC_HOME,
         KC_LSFT,  KC_A,     KC_QUOT,  KC_LBRC,  KC_S,     KC_DOT,   KC_RBRC,  KC_COMM,  KC_V,     KC_G,     KC_U,     KC_SCLN,  KC_RSFT,  KC_RSFT,  KC_UP,
         SH_LCTL,  SH_LWIN,  SH_RWIN,  KC_LALT,  SPC_NAV,  LG_ENG,             LG_RUS,   SPC_NAV,  SH_RALT,  LG_GAME,            KC_LEFT,  KC_DOWN,  KC_RGHT),
@@ -120,9 +129,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NAVIGATION] = LAYOUT_73_jis(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,
-        KC_TAB,  A(KC_F4), C(S(KC_TAB)), KC_TAB, C(KC_TAB), TERMINAL,         KC_ESC,   KC_HOME,  KC_UP,    KC_END,   KC_DEL,   CO_QU_SMART_PAIR,   _______,  _______,
-        KC_ESC,   KC_LWIN, KC_LALT, OSM(MOD_LCTL), KC_LSFT, KC_PSCR,        S(KC_RSFT), KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_BSPC,  CO_EMDASH, _______, KC_ENT,   _______,
-        KC_RSFT,  UNDO,     CUT,      COPY,     PASTE,    REDO1,    REDO2,    G(KC_E),  KC_PGUP,  KC_APP,   KC_PGDN,  KC_ENT,   _______,  _______,  _______,
+        KC_TAB,  A(KC_F4), C(S(KC_TAB)), KC_TAB, C(KC_TAB), TERMINAL,         KC_ESC,   KC_HOME,  KC_UP,    KC_END,   KC_DEL,   CO_QU_SMART_PAIR,   CO_RUB,  _______,
+        KC_ESC,   KC_LWIN, KC_LALT, OSM(MOD_LCTL), KC_LSFT, KC_PSCR,        S(KC_RSFT), KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_BSPC,  CO_EMDASH, CO_DEGREE, KC_ENT,   _______,
+        KC_RSFT,  UNDO,     CUT,      COPY,     PASTE,    REDO1,    REDO2,    G(KC_E),  KC_PGUP,  KC_APP,   KC_PGDN,  KC_ENT,   QU_COMP_PAIR, _______,  _______,
         KC_RCTL,  KC_RWIN,  KC_RCMMD, KC_RALT,  KC_SPC,             LG_ENG,   LG_RUS,             KC_SPC,   KC_RALT,  LG_GAME,            _______,  _______,  _______),
 
     [MACRO] = LAYOUT_73_jis(
@@ -174,90 +183,144 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         return false;
 
-        case SUPER_DOT:  // . ! —
+        case DI_1:
         if (record->event.pressed) {
           clear_oneshot_mods();  // Temporarily disable mods.
           unregister_mods(MOD_MASK_CSAG);
           if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
-                SEND_STRING("!");
-          } else if ((mods | oneshot_mods) & MOD_MASK_CTRL) {
-                tap_code16(KC_RALT);
-                wait_ms(10);
-                tap_code16(KC_MINS);
-                wait_ms(10);
-                tap_code16(KC_MINS);
-                wait_ms(10);
-                tap_code16(KC_MINS);
+            SEND_STRING("}");
           } else {
-             bool is_russian = (get_highest_layer(default_layer_state) == 1);
-             if (is_russian) {
-                tap_code16(LANG_RUS);
-                SEND_STRING("/");
-            } else {
-                tap_code16(LANG_ENG);
-                SEND_STRING(".");
-            }
+            SEND_STRING("1");
           }
           register_mods(mods);  // Restore mods.
         }
         return false;
 
-        case SUPER_COMMA:  // , ? «» “”
+        case DI_2:
         if (record->event.pressed) {
-          bool is_russian = (get_highest_layer(default_layer_state) == 1);
           clear_oneshot_mods();  // Temporarily disable mods.
           unregister_mods(MOD_MASK_CSAG);
           if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
-            if (is_russian) {
-                tap_code16(LANG_RUS);
-                SEND_STRING("&");
-            } else {
-                tap_code16(LANG_ENG);
-                SEND_STRING("?");
-            }
-          } else if ((mods | oneshot_mods) & MOD_MASK_CTRL) {
-            if (is_russian) {
-                tap_code16(LANG_ENG);
-                wait_ms(20);
-                tap_code16(KC_RALT);
-                wait_ms(10);
-                tap_code16(S(KC_COMMA));
-                wait_ms(10);
-                tap_code16(S(KC_COMMA));
-                wait_ms(10);
-                tap_code16(KC_RALT);
-                wait_ms(10);
-                tap_code16(S(KC_DOT));
-                wait_ms(10);
-                tap_code16(S(KC_DOT));
-                wait_ms(10);
-                tap_code16(KC_LEFT);
-                wait_ms(20);
-                tap_code16(LANG_RUS);
-            } else {
-                tap_code16(LANG_ENG);
-                tap_code16(KC_RALT);
-                wait_ms(10);
-                tap_code16(KC_DQUO);
-                wait_ms(10);
-                tap_code16(KC_LT);
-                wait_ms(10);
-                tap_code16(KC_RALT);
-                wait_ms(10);
-                tap_code16(KC_DQUO);
-                wait_ms(10);
-                tap_code16(KC_GT);
-                wait_ms(10);
-                tap_code16(KC_LEFT);
-            }
+            SEND_STRING("M");
           } else {
-            if (is_russian) {
-                tap_code16(LANG_RUS);
-                SEND_STRING("?");
-            } else {
-                tap_code16(LANG_ENG);
-                SEND_STRING(",");
-            }
+            SEND_STRING("2");
+          }
+          register_mods(mods);  // Restore mods.
+        }
+        return false;
+
+        case DI_4:
+        if (record->event.pressed) {
+          clear_oneshot_mods();  // Temporarily disable mods.
+          unregister_mods(MOD_MASK_CSAG);
+          if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+            SEND_STRING("%");
+          } else {
+            SEND_STRING("4");
+          }
+          register_mods(mods);  // Restore mods.
+        }
+        return false;
+
+        case DI_5:
+        if (record->event.pressed) {
+          clear_oneshot_mods();  // Temporarily disable mods.
+          unregister_mods(MOD_MASK_CSAG);
+          if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+            SEND_STRING("^");
+          } else {
+            SEND_STRING("5");
+          }
+          register_mods(mods);  // Restore mods.
+        }
+        return false;
+
+        case DI_6:
+        if (record->event.pressed) {
+          clear_oneshot_mods();  // Temporarily disable mods.
+          unregister_mods(MOD_MASK_CSAG);
+          if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+            SEND_STRING("$");
+          } else {
+            SEND_STRING("6");
+          }
+          register_mods(mods);  // Restore mods.
+        }
+        return false;
+
+        case DI_7:
+        if (record->event.pressed) {
+          clear_oneshot_mods();  // Temporarily disable mods.
+          unregister_mods(MOD_MASK_CSAG);
+          if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+            SEND_STRING("-");
+          } else {
+            SEND_STRING("7");
+          }
+          register_mods(mods);  // Restore mods.
+        }
+        return false;
+
+        case DI_8:
+        if (record->event.pressed) {
+          clear_oneshot_mods();  // Temporarily disable mods.
+          unregister_mods(MOD_MASK_CSAG);
+          if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+            SEND_STRING("@");
+          } else {
+            SEND_STRING("8");
+          }
+          register_mods(mods);  // Restore mods.
+        }
+        return false;
+
+        case DI_STAR:
+        if (record->event.pressed) {
+          clear_oneshot_mods();  // Temporarily disable mods.
+          unregister_mods(MOD_MASK_CSAG);
+          if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+            SEND_STRING("_");
+          } else {
+            SEND_STRING("*");
+          }
+          register_mods(mods);  // Restore mods.
+        }
+        return false;
+
+        case DI_DOT:
+        if (record->event.pressed) {
+          clear_oneshot_mods();  // Temporarily disable mods.
+          unregister_mods(MOD_MASK_CSAG);
+          if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+            SEND_STRING("!");
+          } else {
+            SEND_STRING("/");
+          }
+          register_mods(mods);  // Restore mods.
+        }
+        return false;
+
+        case DI_COMMA:
+        if (record->event.pressed) {
+          clear_oneshot_mods();  // Temporarily disable mods.
+          unregister_mods(MOD_MASK_CSAG);
+          if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+            SEND_STRING("&");
+          } else {
+            SEND_STRING("?");
+          }
+          register_mods(mods);  // Restore mods.
+        }
+        return false;
+
+        case DI_HARD:
+        if (record->event.pressed) {
+          clear_oneshot_mods();  // Temporarily disable mods.
+          unregister_mods(MOD_MASK_CSAG);
+          if ((mods | oneshot_mods) & MOD_MASK_SHIFT) {
+            SEND_STRING("]");
+          } else {
+            SEND_STRING("m");
           }
           register_mods(mods);  // Restore mods.
         }
