0f5300b08243a315f6d29bbf9b1ac221b205fdc9
Petr <petr@duck.com>
2026-01-14 22:18:17 +0300

    refactor - to switch/case

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
@@ -191,41 +191,49 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         static deferred_token token = INVALID_DEFERRED_TOKEN;
         static report_mouse_t report = {0};
+
         if (token) {
             // If jiggler is currently running, stop when any key is pressed.
             cancel_deferred_exec(token);
-            token = INVALID_DEFERRED_TOKEN;
+            token  = INVALID_DEFERRED_TOKEN;
             report = (report_mouse_t){};  // Clear the mouse.
             host_mouse_send(&report);
-        } else if (keycode == JIGGLE) {
-            uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
-                // Deltas to move in a circle of radius 20 pixels over 32 frames.
-                static const int8_t deltas[32] = {
-                    0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
-                    0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
-                static uint8_t phase = 0;
-                // Get x delta from table and y delta by rotating a quarter cycle.
-                report.x = deltas[phase];
-                report.y = deltas[(phase + 8) & 31];
-                phase = (phase + 1) & 31;
-                host_mouse_send(&report);
-                return 16;  // Call the callback every 16 ms.
+        } else {
+            switch (keycode) {
+                case JIGGLE: {
+                    uint32_t jiggler_callback(uint32_t trigger_time, void *cb_arg) {
+                        // Deltas to move in a circle of radius 20 pixels over 32 frames.
+                        static const int8_t deltas[32] = {
+                            0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
+                            0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0
+                        };
+                        static uint8_t phase = 0;
+                        // Get x delta from table and y delta by rotating a quarter cycle.
+                        report.x = deltas[phase];
+                        report.y = deltas[(phase + 8) & 31];
+                        phase    = (phase + 1) & 31;
+                        host_mouse_send(&report);
+                        return 16;  // Call the callback every 16 ms.
+                    }
+                    token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
+                } break;
+
+                case IN_ROW_WIN:
+                    tap_code(KC_END);
+                    tap_code16(LSFT(KC_ENT));
+                    return false;
+
+                case IN_ROW_MAC:
+                    tap_code16(LGUI(KC_RGHT));
+                    tap_code16(LSFT(KC_ENT));
+                    return false;
+
+                default:
+                    break;
             }
-            token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
-        } else if (keycode == IN_ROW_WIN) {
-            if (record->event.pressed) {
-                tap_code(KC_END);
-                tap_code16(LSFT(KC_ENT));
-            }
-            return false;
-        } else if (keycode == IN_ROW_MAC) {
-            if (record->event.pressed) {
-                tap_code16(LGUI(KC_RGHT));
-                tap_code16(LSFT(KC_ENT));
-            }
-            return false;
         }
     }
+
     return true;
 }
 
