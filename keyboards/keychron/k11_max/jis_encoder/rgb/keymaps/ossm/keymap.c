bdd428c1a0a587f728a8c247fff1368586fda142
Petr <petr@duck.com>
2026-01-14 22:09:08 +0300

    WORKING - macros fixed

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
@@ -214,12 +214,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
         } else if (keycode == IN_ROW_WIN) {
             if (record->event.pressed) {
-                SEND_STRING(SS_TAP(X_END) SS_LSFT(KC_ENT));
+                tap_code(KC_END);
+                tap_code16(LSFT(KC_ENT));
             }
             return false;
         } else if (keycode == IN_ROW_MAC) {
             if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(KC_RGHT) SS_LSFT(KC_ENT));
+                tap_code16(LGUI(KC_RGHT));
+                tap_code16(LSFT(KC_ENT));
             }
             return false;
         }
