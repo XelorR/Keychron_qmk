29354836783502f854d2ee64ffa2dc378e0fb6fe
Petr <petr@duck.com>
2026-01-14 16:38:36 +0300

    attempt to built into JIGGLER logic

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
@@ -53,6 +53,9 @@ enum custom_keycodes {
     OS_CTRL,
     OS_ALT,
     OS_CMD,
+
+    IN_ROW_WIN,
+    IN_ROW_MAC,
 };
 
 // clang-format off
@@ -94,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_NAV] = LAYOUT_73_jis(
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,
-        _______,  KC_DEL,   WN_TABL,  KC_TAB,   WN_TABR,  KC_LGUI,  KC_INS,   _______,  _______,  _______,  KC_DEL,   _______,  _______,                      _______,
+        _______,  KC_DEL,   WN_TABL,  KC_TAB,   WN_TABR,  KC_LGUI,  KC_INS,   _______,  _______,  IN_ROW_WIN, KC_DEL, _______,  _______,                      _______,
         _______,  OS_CMD,   OS_ALT,   OS_CTRL,  OS_SHFT,  KC_PSCR,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_BSPC,  _______,  _______,  WN_SNAP,  _______,
         _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  C(KC_Y),  BAT_LVL,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,  _______,            _______,  _______,  _______,            _______,  _______,  _______),
@@ -195,24 +198,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         report = (report_mouse_t){};  // Clear the mouse.
         host_mouse_send(&report);
         } else if (keycode == JIGGLE) {
-
-        uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
-            // Deltas to move in a circle of radius 20 pixels over 32 frames.
-            static const int8_t deltas[32] = {
-                0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
-                0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
-            static uint8_t phase = 0;
-            // Get x delta from table and y delta by rotating a quarter cycle.
-            report.x = deltas[phase];
-            report.y = deltas[(phase + 8) & 31];
-            phase = (phase + 1) & 31;
-            host_mouse_send(&report);
-            return 16;  // Call the callback every 16 ms.
-        }
-
+            uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
+                // Deltas to move in a circle of radius 20 pixels over 32 frames.
+                static const int8_t deltas[32] = {
+                    0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
+                    0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
+                static uint8_t phase = 0;
+                // Get x delta from table and y delta by rotating a quarter cycle.
+                report.x = deltas[phase];
+                report.y = deltas[(phase + 8) & 31];
+                phase = (phase + 1) & 31;
+                host_mouse_send(&report);
+                return 16;  // Call the callback every 16 ms.
+            }
         token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
+        } else if (keycode == IN_ROW_WIN) {
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_END) SS_LSFT(X_ENT));
+            }
+        }
     }
-  }
     return true;
 }
 
