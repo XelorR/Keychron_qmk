54e15c6a7a5ac2ea1eef9555c75b72d2e16ea9be
Petr <petr@duck.com>
2026-01-18 22:50:58 +0300

    3layerstate for ru added

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
@@ -244,6 +244,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     state = update_tri_layer_state(state, SYM, NAV, NUM);
+    state = update_tri_layer_state(state, SYM_RU, NAV, NUM);
     return state;
 }
 
