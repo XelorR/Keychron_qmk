4a818a52a1e9bafe1515717ad18c9fe60a6ae3bb
Petr <petr@duck.com>
2026-08-20 22:05:56 +0300

    trilayer changed to num

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
@@ -477,7 +477,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    state = update_tri_layer_state(state, MAC_SYM, MAC_NAV, MAC_FUN);
-    state = update_tri_layer_state(state, WIN_SYM, WIN_NAV, WIN_FUN);
+    state = update_tri_layer_state(state, MAC_SYM, MAC_NAV, MAC_NUM);
+    state = update_tri_layer_state(state, WIN_SYM, WIN_NAV, WIN_NUM);
     return state;
 }
