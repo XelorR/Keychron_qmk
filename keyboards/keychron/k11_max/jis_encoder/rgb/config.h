cfcf830dc0268a37275510839bcf0199860e3e23
Petr <petr@duck.com>
2026-08-24 22:12:34 +0300

    miryoku config.h added

--- keyboards/keychron/k11_max/jis_encoder/rgb/config.h
+++ keyboards/keychron/k11_max/jis_encoder/rgb/config.h
@@ -45,8 +45,29 @@
 
 #endif
 
+#undef TAPPING_TERM
 #define TAPPING_TERM 200
-#define QUICK_TAP_TERM 100
-#define HOLD_ON_OTHER_KEY_PRESS
 
-#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
+
