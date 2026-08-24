48aedf67e026c0c8f19845507942a19e04812c2d
Petr <petr@duck.com>
2026-08-24 19:47:25 +0300

    button pasted as trans for all buttons -- to use as draft

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
@@ -122,6 +122,8 @@ enum custom_keycodes {
     MINS_5,  // 5x minus
 };
 
+// _______
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [U_BASE] = LAYOUT_73_jis(
@@ -130,18 +132,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(U_MEDIA,KC_ESC),LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_SCLN),      KC_QUOT,  KC_ENT,   KC_ENT,   KC_HOME,
         KC_LSFT,           LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,     KC_B,    KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH), KC_RSFT,  KC_RSFT,  KC_UP,
         KC_LCTL,           KC_LCTL,           KC_LWIN,           KC_LALT,           LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), KC_RALT,           KC_RCTL,                                           KC_LEFT,  KC_DOWN,  KC_RGHT),
+
     [U_EXTRA] = LAYOUT_73_jis(
         KC_ESC,            KC_1,              KC_2,              KC_3,              KC_4,              KC_5,              KC_6,              KC_7,              KC_8,              KC_9,              KC_0,                 KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSPC,  KC_MUTE,
         KC_TAB,            KC_Q,              KC_W,              KC_F,              KC_P,              KC_G,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_SCLN,              KC_LBRC,  KC_RBRC,            KC_DEL,
         LT(U_MEDIA,KC_ESC),LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      KC_D,              KC_H,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),         KC_QUOT,  KC_ENT,   KC_ENT,   KC_HOME,
         KC_LSFT,           LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,     KC_B,    KC_K,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH), KC_RSFT,  KC_RSFT,  KC_UP,
         KC_LCTL,           KC_LCTL,           KC_LWIN,           KC_LALT,           LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), KC_RALT,           KC_RCTL,                                           KC_LEFT,  KC_DOWN,  KC_RGHT),
+
     [U_TAP] = LAYOUT_73_jis(
         KC_ESC,            KC_1,              KC_2,              KC_3,              KC_4,              KC_5,              KC_6,              KC_7,              KC_8,              KC_9,              KC_0,                 KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSPC,  KC_MUTE,
         KC_TAB,            KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,                 KC_LBRC,  KC_RBRC,            KC_DEL,
         KC_CAPS,           KC_A,              KC_S,              KC_D,              KC_F,              KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_SCLN,              KC_QUOT,  KC_ENT,   KC_ENT,   KC_HOME,
         KC_LSFT,           KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,     KC_B,    KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,              KC_RSFT,  KC_RSFT,  KC_UP,
         KC_LCTL,           KC_LCTL,           KC_LWIN,           KC_LALT,           KC_SPC,            LT(U_NAV,KC_TAB),  LT(U_NUM,KC_ENT),  KC_SPC,            KC_RALT,           KC_RCTL,                                           KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [U_BUTTON] = LAYOUT_73_jis(
+        _______,           _______,           _______,           _______,           _______,           _______,          _______,            _______,           _______,           _______,           _______,              _______,  _______,  _______,  _______,  _______,
+        _______,           _______,           _______,           _______,           _______,           _______,          _______,            _______,           _______,           _______,           _______,              _______,  _______,            _______,
+        _______,           _______,           _______,           _______,           _______,           _______,          _______,            _______,           _______,           _______,           _______,              _______,  _______,  _______,  _______,
+        _______,           _______,           _______,           _______,           _______,           _______, _______, _______,            _______,           _______,           _______,           _______,              _______,  _______,  _______,
+        _______,           _______,           _______,           _______,           _______,           _______,          _______,            _______,           _______,           _______,                                           _______,  _______,  _______),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
