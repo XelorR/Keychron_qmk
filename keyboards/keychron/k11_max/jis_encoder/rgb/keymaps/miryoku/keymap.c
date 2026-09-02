81f4faf20ac4687d029ce67eb193dca67ca1b5f2
Petr <petr@duck.com>
2026-09-02 17:43:39 +0300

    gam fn -- right side is now mirrorring nav layer

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/miryoku/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/miryoku/keymap.c
@@ -199,9 +199,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [U_GAM_FN] = LAYOUT_73_jis(
         _______,           KC_F1,             KC_F2,             KC_F3,             KC_F4,             KC_F5,             KC_F6,             KC_F7,             KC_F8,             KC_F9,             KC_F10,               KC_F11,   KC_F12,   _______,  _______,  RGB_TOG,
-        KC_J,              KC_DOT,            KC_7,              KC_8,              KC_9,              KC_GRV,            U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,                _______,  _______,            _______,
-        KC_I,              KC_COMM,           KC_4,              KC_5,              KC_6,              KC_H,              CW_TOGG,           KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,              _______,  _______,  _______,  _______,
-        KC_M,              KC_SLSH,           KC_1,              KC_2,              KC_3,              KC_MUTE, BAT_LVL,  KC_INS,            KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,               _______,  _______,  _______,
+        KC_J,              KC_DOT,            KC_7,              KC_8,              KC_9,              KC_GRV,            KC_INS,            KC_HOME,           KC_UP,             KC_END,            KC_PGUP,              _______,  _______,            _______,
+        KC_I,              KC_COMM,           KC_4,              KC_5,              KC_6,              KC_H,              CW_TOGG,           KC_LEFT,           KC_DOWN,           KC_RGHT,           KC_PGDN,              _______,  _______,  _______,  _______,
+        KC_M,              KC_SLSH,           KC_1,              KC_2,              KC_3,              KC_MUTE, BAT_LVL,  U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,                _______,  _______,  _______,
         _______,           _______,           _______,           KC_ENT,            KC_0,              _______,           DF(U_BASE),        KC_PSCR,           U_NA,              _______,                                           _______,  _______,  _______),
 
 };
