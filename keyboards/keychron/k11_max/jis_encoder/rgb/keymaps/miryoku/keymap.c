e817cf38bb7e9c350eafa8245a6dd5dca6d610ca
Petr <petr@duck.com>
2026-09-03 16:05:38 +0300

    gaming fn switched to mouse, additional sym extra added

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/miryoku/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/miryoku/keymap.c
@@ -57,8 +57,8 @@ enum layers {
     U_MEDIA,
     U_NUM,
     U_SYM,
+    U_SYM_EXTRA,
     U_FUN,
-    U_GAM_FN,
 };
 
 enum {
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,            KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,                 KC_LBRC,  KC_RBRC,            KC_DEL,
         KC_CAPS,           KC_A,              KC_S,              KC_D,              KC_F,              KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_SCLN,              KC_QUOT,  KC_ENT,   KC_ENT,   KC_HOME,
         KC_LSFT,           KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,     KC_B,    KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,              KC_RSFT,  KC_RSFT,  KC_UP,
-        KC_LCTL,           U_CMD,             U_WIN,             U_ALT,             KC_SPC,            MO(U_GAM_FN),      LT(U_NUM,KC_ENT),  KC_SPC,            KC_RALT,           U_CMD,                                             KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,           U_CMD,             U_WIN,             U_ALT,             KC_SPC,            MO(U_MOUSE),       LT(U_NUM,KC_ENT),  KC_SPC,            KC_RALT,           U_CMD,                                             KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [U_BUTTON] = LAYOUT_73_jis(
         _______,           _______,           _______,           _______,           _______,           _______,           _______,           _______,           _______,           _______,           _______,              _______,  _______,  _______,  _______,  _______,
@@ -220,13 +220,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,           KC_F10,            KC_F1,             KC_F2,             KC_F3,             KC_PAUS, BAT_LVL,  U_NA,              TD(U_TD_U_FUN),    TD(U_TD_U_MEDIA),  KC_ALGR,           U_NA,                 _______,  _______,  _______,
         _______,           _______,           _______,           KC_APP,            KC_SPC,            KC_TAB,            U_NA,              U_NA,              U_NA,              _______,                                           _______,  _______,  _______),
 
-    [U_GAM_FN] = LAYOUT_73_jis(
-        _______,           KC_F1,             KC_F2,             KC_F3,             KC_F4,             KC_F5,             KC_F6,             KC_F7,             KC_F8,             KC_F9,             KC_F10,               KC_F11,   KC_F12,   _______,  _______,  RGB_TOG,
-        KC_J,              KC_DOT,            KC_7,              KC_8,              KC_9,              KC_GRV,            KC_INS,            KC_HOME,           KC_UP,             KC_END,            KC_PGUP,              _______,  _______,            _______,
-        KC_I,              KC_COMM,           KC_4,              KC_5,              KC_6,              KC_H,              CW_TOGG,           KC_LEFT,           KC_DOWN,           KC_RGHT,           KC_PGDN,              _______,  _______,  _______,  _______,
-        KC_M,              KC_SLSH,           KC_1,              KC_2,              KC_3,              KC_MUTE, BAT_LVL,  U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,                _______,  _______,  _______,
-        _______,           _______,           _______,           KC_ENT,            KC_0,              _______,           DF(U_BASE),        KC_PSCR,           U_NA,              _______,                                           _______,  _______,  _______),
-
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -239,9 +232,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [U_MOUSE] = { ENCODER_CCW_CW(BSPC_5, MINS_5)},
         [U_MEDIA]  = { ENCODER_CCW_CW(BSPC_5, MINS_5)},
         [U_SYM]  = { ENCODER_CCW_CW(U_UND, U_RDO)},
+        [U_SYM_EXTRA]  = { ENCODER_CCW_CW(U_UND, U_RDO)},
         [U_NUM]  = { ENCODER_CCW_CW(U_UND, U_RDO)},
         [U_FUN]  = { ENCODER_CCW_CW(U_UND, U_RDO)},
-        [U_GAM_FN]  = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     };
 #endif // ENCODER_MAP_ENABLE
 
