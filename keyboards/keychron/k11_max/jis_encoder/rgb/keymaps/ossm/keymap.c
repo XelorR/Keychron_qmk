b796df5180de3a33b73460e7be4e24458cc51854
Petr <petr@duck.com>
2026-09-08 20:22:02 +0300

    bootloader button added

--- keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
+++ keyboards/keychron/k11_max/jis_encoder/rgb/keymaps/ossm/keymap.c
@@ -173,14 +173,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  MS_BTN1,  MS_BTN2,            _______,  _______,            _______,  _______,            _______,  _______,  _______),
 
     [MAC_FUN] = LAYOUT_73_jis(
-        KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,            RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  RGB_TOG,
+        QK_BOOT,  KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,            RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  RGB_TOG,
         _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    KC_MPLY,            MC_LOCK,  KC_F7,    KC_F8,    KC_F9,    KC_F12,   _______,  _______,            _______,
         CL_CTRL,  OS_CTRL,  OS_ALT,   OS_CMD,   OS_SHFT,  CW_TOGG,            MC_PSCR,  KC_F4,    KC_F5,    KC_F6,    KC_F11,   EC_SWAP,  _______,  _______,  _______,
         CL_CAPS,  JIGGLE,   KC_VOLD,  KC_MUTE,  KC_VOLU,  MC_KILL,  BAT_LVL,  MC_APP,   KC_F1,    KC_F2,    KC_F3,    KC_F10,   EC_NORM,  _______,  _______
         _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,            _______,  _______,            _______,  _______,  _______),
 
     [WIN_FUN] = LAYOUT_73_jis(
-        KC_ESC,   KC_BRID,  KC_BRIU,  KC_TASK,  KC_MYCM,  RGB_VAD,            RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  RGB_TOG,
+        QK_BOOT,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_MYCM,  RGB_VAD,            RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  RGB_TOG,
         _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    KC_MPLY,            WN_LOCK,  KC_F7,    KC_F8,    KC_F9,    KC_F12,   _______,  _______,            _______,
         CL_CTRL,  OS_CMD,   OS_ALT,   OS_CTRL,  OS_SHFT,  CW_TOGG,            KC_PSCR,  KC_F4,    KC_F5,    KC_F6,    KC_F11,   EC_SWAP,  _______,  _______,  _______,
         CL_CAPS,  JIGGLE,   KC_VOLD,  KC_MUTE,  KC_VOLU,  WN_KILL,  BAT_LVL,  KC_APP,   KC_F1,    KC_F2,    KC_F3,    KC_F10,   EC_NORM,  _______,  _______
